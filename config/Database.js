4fe6871ba3c888d313d4d9822f710ae06fce4f8a
Elghiffari28 <elbag467@gmail.com>
2025-03-13 01:21:04 +0700

    Finishing

--- config/Database.js
+++ config/Database.js
@@ -1,11 +1,14 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const db = new Sequelize(
   process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.DB_USER || "root",
+  process.env.DB_PASSWORD || "",
   {
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST || "localhost",
     dialect: process.env.DB_DIALECT || "mysql",
     logging: false, // Hapus logging agar lebih clean
   }
